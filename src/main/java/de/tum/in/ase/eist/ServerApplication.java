fb89f13872276cd4c41824463f38cbc04bbbebf7
sophi <ge65guh@mytum.de>
2022-07-14 13:35:58 +0200

    test

--- src/main/java/de/tum/in/ase/eist/ServerApplication.java
+++ src/main/java/de/tum/in/ase/eist/ServerApplication.java
@@ -2,7 +2,9 @@ package de.tum.in.ase.eist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//
+
+
+
 @SpringBootApplication
 public class ServerApplication {
 
